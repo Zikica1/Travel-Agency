2596267c34c25effe03e07ff347352028d2ae1cd
Žikica Drača <zikica.draca@gmail.com>
2023-06-01 17:10:09 +0200

    change index.js

--- src/js/index.js
+++ src/js/index.js
@@ -81,7 +81,7 @@ testimonialCards.forEach((card) => {
 let sliderImages = document.querySelectorAll('.slide');
 let arrowLeft = document.querySelector('.arrow-left');
 let arrowRight = document.querySelector('.arrow-right');
-current = 0;
+let current = 0;
 
 function reset() {
   for (let i = 0; i < sliderImages.length; i++) {
